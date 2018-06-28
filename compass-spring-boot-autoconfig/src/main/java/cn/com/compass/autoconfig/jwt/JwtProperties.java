490be3539f9a6f58ec522aa6e3968847291c61d2
wanmk <524623302@qq.com>
2018-06-28 13:08:37 +0800

    compass-spring-boot优化
    自动化配置META-INF/spring.factories配置错误

---
+++ compass-spring-boot-autoconfig/src/main/java/cn/com/compass/autoconfig/jwt/JwtProperties.java
@@ -0,0 +1,46 @@
+package cn.com.compass.autoconfig.jwt;
+
+import java.util.UUID;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
+/**
+ * 
+ * @author wanmk
+ * @git https://gitee.com/milkove
+ * @email 524623302@qq.com
+ * @todo jwt属性
+ * @date 2018年6月6日 下午3:33:08
+ *
+ */
+@Data
+@ConfigurationProperties(prefix = JwtProperties.prefix)
+public class JwtProperties {
+	public static final String prefix = "jwt";
+	
+	private boolean enabled = true;
+	/**
+     * JWT 生成密钥使用的密码
+     */
+    private String secret = "cn.com.compass";
+    
+    /**
+     * 过期时间 默认二个小时
+     */
+    private long expire = 2*60*60*1000; 
+    
+    /**
+     * 签发方
+     */
+    private String iss = "compass";
+    /**
+     * 接收方
+     */
+    private String aud = "anyone";
+    /**
+     * 唯一标识
+     */
+    private String jti = UUID.randomUUID().toString();
+	
+}
