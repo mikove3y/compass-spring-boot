b5956ce042b6528f8acd4e077f12623e06abd943
wanmk <524623302@qq.com>
2018-09-10 23:11:56 +0800

    compass优化
    1、优化camel错误输出

--- compass-spring-boot-camel/src/main/java/cn/com/compass/camel/router/base/BaseErrorProcessor.java
+++ compass-spring-boot-camel/src/main/java/cn/com/compass/camel/router/base/BaseErrorProcessor.java
@@ -3,8 +3,11 @@ package cn.com.compass.camel.router.base;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
+import cn.com.compass.autoconfig.constant.ConstantUtil;
+import cn.com.compass.base.constant.BaseConstant;
 import cn.com.compass.base.vo.BaseResponseVo;
 import cn.com.compass.util.ExceptionUtil;
+import cn.com.compass.web.context.AppContext;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -14,9 +17,10 @@ public class BaseErrorProcessor implements Processor {
 	 */
 	@Override
 	public void process(Exchange exchange) throws Exception {
+		ConstantUtil constant = AppContext.getInstance().getBean(ConstantUtil.class);
 		Exception e = exchange.getProperty(Exchange.EXCEPTION_CAUGHT, Exception.class);
 		log.error(ExceptionUtil.stackTraceToString(e,BaseErrorProcessor.class.getName()));
-		exchange.getOut().setBody(BaseResponseVo.success());
+		exchange.getOut().setBody(new BaseResponseVo(BaseConstant.INNER_ERRO, constant.getValue(BaseConstant.INNER_ERRO),e));
 	}
 
 }
