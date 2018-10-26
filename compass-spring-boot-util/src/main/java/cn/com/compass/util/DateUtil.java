e046962b552fec1a82f0d3181795207c833ad924
wanmk <524623302@qq.com>
2018-10-26 10:53:31 +0800

    compass优化
    util模块dateUtil 优化12小时制为24小时制

--- compass-spring-boot-util/src/main/java/cn/com/compass/util/DateUtil.java
+++ compass-spring-boot-util/src/main/java/cn/com/compass/util/DateUtil.java
@@ -31,14 +31,14 @@ public class DateUtil {
 
 	public static final long YEAR = DAY * 365; // 一年
 
-	// 日期时间类型格式
-	public static final String DATETIME_FORMAT = "yyyy-MM-dd hh:mm:ss";
+	// 日期时间类型格式 24小时制HH:mm:ss 12小时制hh:mm:ss
+	public static final String DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
 	// 日期类型格式
 	public static final String DATE_FORMAT = "yyyy-MM-dd";
 
-	// 时间类型的格式
-	public static final String TIME_FORMAT = "hh:mm:ss";
+	// 时间类型的格式 24小时制HH:mm:ss 12小时制hh:mm:ss
+	public static final String TIME_FORMAT = "HH:mm:ss";
 
 	public static final Map<Integer, String> WEEK_DAY = new HashMap<>();
 	
