ba7df094b4d21f22234d59906d761424da7e2e5b
wanmk <524623302@qq.com>
2018-12-24 18:39:43 +0800

    优化feign请求拦截器
    1、java.lang.IllegalStateException: No thread-bound request found: Are you referring to request attributes outside of an actual web request,报错处理

--- compass-spring-boot-web/src/main/java/cn/com/compass/web/feign/FeignRequestInterceptor.java
+++ compass-spring-boot-web/src/main/java/cn/com/compass/web/feign/FeignRequestInterceptor.java
@@ -4,6 +4,7 @@ import cn.com.compass.base.constant.BaseConstant;
 import cn.com.compass.web.context.GlobalContext;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -18,6 +19,7 @@ import java.util.regex.Pattern;
  * @date 2018/12/3 11:36
  */
 @Component
+@Slf4j
 public class FeignRequestInterceptor implements RequestInterceptor {
     /**
      * 全局上线文
@@ -36,8 +38,21 @@ public class FeignRequestInterceptor implements RequestInterceptor {
          * 1、服务间调用A->B B并不会把相应的api暴露出去的情况下，不要使用版本管理
          */
         // 拦截三个参数 MessageId Authorization SystemCode
-        template.header(BaseConstant.SYSTEMCODE, context.getCurrentUserSystemCode());
-        template.header(BaseConstant.MESSAGEID, context.getCurentUserMessageId());
-        template.header(BaseConstant.AUTHORIZATION_KEY, context.getCurrentUserToken());
+        // 判断是否是当前线程请求
+        /**
+         *
+         * java.lang.IllegalStateException: No thread-bound request found: Are you referring to request attributes outside of an actual web request,
+         * or processing a request outside of the originally receiving thread?
+         * If you are actually operating within a web request and still receive this message,
+         * your code is probably running outside of DispatcherServlet/DispatcherPortlet: In this case, use RequestContextListener or RequestContextFilter to expose the current request.
+         *
+         */
+        try{
+            template.header(BaseConstant.SYSTEMCODE, context.getCurrentUserSystemCode());
+            template.header(BaseConstant.MESSAGEID, context.getCurentUserMessageId());
+            template.header(BaseConstant.AUTHORIZATION_KEY, context.getCurrentUserToken());
+        }catch (Exception e){
+            log.error(e.getMessage());
+        }
     }
 }
